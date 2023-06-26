7ff1f3b5798f570714c1e34fda56e1b3f3a5ea67
omihasan1307 <omihasan1307@gmail.com>
2023-06-26 22:43:03 +0600

    user data post done

--- index.js
+++ index.js
@@ -2,9 +2,55 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.port || 5000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9nztkwc.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const userCollection = client.db("melodyDb").collection("users");
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const query = { email: user.email };
+      const existingsUser = await userCollection.findOne(query);
+      if (!existingsUser) {
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      }
+    });
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("summer camping ");
