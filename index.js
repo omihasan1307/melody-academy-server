55833da80ee149439a29fd842c75122f0573ecd7
omihasan1307 <omihasan1307@gmail.com>
2023-06-30 17:07:09 +0600

    jwt access

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = process.env.port || 5000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 
 app.use(cors());
 app.use(express.json());
@@ -18,18 +19,54 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyJwt = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, process.env.ACCESS_KEY, function (err, decoded) {
+      if (err) {
+        res.send({ error: true, message: " unauthorized access" });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    res.send({ error: true, message: " unauthorized access" });
+  }
+};
+
 async function run() {
   try {
     const userCollection = client.db("melodyDb").collection("users");
 
-    app.get("/users", async (req, res) => {
+    const verifyRole = async (req, res, next) => {
+      const isExist = await userCollection.findOne({
+        email: req.query.email,
+      });
+      if (isExist.role === "admin" || isExist.role === "instructor") {
+        req.query.role = isExist?.role;
+      }
+      res.send({ role: isExist.role });
+    };
+
+    app.post("/jwt", (req, res) => {
+      const body = req.body;
+      const token = jwt.sign(body, process.env.ACCESS_KEY, { expiresIn: "1h" });
+      res.send({ token });
+    });
+
+    app.get("/users", verifyJwt, async (req, res) => {
+      console.log(req.decoded);
+      console.log(req.query);
+      // if (req.decoded.email === req.query.email) {
+      // }
       const result = await userCollection.find().toArray();
       res.send(result);
+      // res.send({ message: "forbidden access" });
     });
 
     app.post("/users", async (req, res) => {
       const user = req.body;
-      console.log(user);
       const query = { email: user.email };
       const existingsUser = await userCollection.findOne(query);
       if (!existingsUser) {
